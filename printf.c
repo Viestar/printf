3b04824f7c09204080245887677ed06dda933924
Viestar <m.silverbuckson@gmail.com>
2023-03-27 07:24:17 +0300

    printf.c

---
+++ printf.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* _printf - function that produces output according to @format
+* @format: format specifier
+* Return: ADD RETURN PARTNER
+*/
+
+int _printf(const char *format, ...)
+{
+    /* Using va_list data type from stdarg.h to store arguments */
+	va_list arguments;
+}
